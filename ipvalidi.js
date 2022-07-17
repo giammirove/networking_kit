ff87105952826ca7790805b138ea57f8fd18531d
giammirove <rovelli.gianmaria@gmail.com>
2022-07-17 12:32:39 +0200

    wow

--- ipvalidi.js
+++ ipvalidi.js
@@ -8,9 +8,22 @@ function execute(ip, cidr) {
   if (!utility.valid(ip)) {
     console.log("[x] Non valido !");
   } else {
+    let bin = utility.ip_to_bin(ip);
+    let bin2 = "";
+    let bin3 = "";
+    for (let i = 0; i < 32; i++) {
+      bin2 += bin[i];
+      bin3 += bin[i];
+      if ((i + 1) % 8 == 0) bin3 += " ";
+      if (i == cidr - 1) bin3 += "|";
+      if (i == cidr - 1) bin2 += " ";
+    }
+    console.log(`[-] Ben formattato\n\t${bin3}`);
+    let host = bin2.split(" ")[1].padStart(32, "0");
+    console.log(`[-] Binario\n\t${bin2}`);
     console.log("[-] E' valido");
     if (utility.is_host(ip, cidr)) {
-      console.log("[-] E' host");
+      console.log(`[-] E' host \n\t${host}\n\t${utility.bit_to_ip(host)}`);
     } else {
       console.log("[-] E' rete");
     }
